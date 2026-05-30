0c5c363b2aca696c4fc605bcb2bd3f0cbc76b66a
Rikz Amman <rikanamman5@gmail.com>
2026-05-29 17:24:35 -1200

    fix: monkey-patch SSL context for TLS 1.2 + pin Python 3.12

--- app/database/mongo.py
+++ app/database/mongo.py
@@ -2,13 +2,16 @@ import ssl
 from motor.motor_asyncio import AsyncIOMotorClient
 from core.config import MONGO_URI, DB_NAME
 
-tls_ctx = ssl.create_default_context()
-tls_ctx.check_hostname = False
-tls_ctx.verify_mode = ssl.CERT_NONE
-try:
-    tls_ctx.minimum_version = ssl.TLSVersion.TLSv1_2
-    tls_ctx.maximum_version = ssl.TLSVersion.TLSv1_2
-except AttributeError:
-    pass
-client = AsyncIOMotorClient(MONGO_URI, tls_context=tls_ctx, tls=True)
+_original_create_default_context = ssl.create_default_context
+def _patched_create_default_context(*args, **kwargs):
+    ctx = _original_create_default_context(*args, **kwargs)
+    try:
+        ctx.minimum_version = ssl.TLSVersion.TLSv1_2
+        ctx.maximum_version = ssl.TLSVersion.TLSv1_2
+    except AttributeError:
+        pass
+    return ctx
+ssl.create_default_context = _patched_create_default_context
+
+client = AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
