19d6812a8f95c8bd16bdab65bd79aafc79358742
Rikz Amman <rikanamman5@gmail.com>
2026-05-29 17:08:30 -1200

    fix: force TLS 1.2 for MongoDB Atlas compatibility

--- app/database/mongo.py
+++ app/database/mongo.py
@@ -1,5 +1,14 @@
+import ssl
 from motor.motor_asyncio import AsyncIOMotorClient
 from core.config import MONGO_URI, DB_NAME
 
-client = AsyncIOMotorClient(MONGO_URI)
+tls_ctx = ssl.create_default_context()
+tls_ctx.check_hostname = False
+tls_ctx.verify_mode = ssl.CERT_NONE
+try:
+    tls_ctx.minimum_version = ssl.TLSVersion.TLSv1_2
+    tls_ctx.maximum_version = ssl.TLSVersion.TLSv1_2
+except AttributeError:
+    pass
+client = AsyncIOMotorClient(MONGO_URI, ssl_context=tls_ctx, tls=True)
 db = client[DB_NAME]
