dc54b064663b5206c94156be2ff8970372be163d
Rikz Amman <rikanamman5@gmail.com>
2026-05-29 17:32:01 -1200

    fix: append tlsInsecure to MongoDB URI

--- app/database/mongo.py
+++ app/database/mongo.py
@@ -1,18 +1,18 @@
 import ssl
+from urllib.parse import urlparse, urlunparse, parse_qs, urlencode
 from motor.motor_asyncio import AsyncIOMotorClient
 from core.config import MONGO_URI, DB_NAME
 
-_original_create_default_context = ssl.create_default_context
-def _patched_create_default_context(*args, **kwargs):
-    ctx = _original_create_default_context(*args, **kwargs)
-    try:
-        ctx.minimum_version = ssl.TLSVersion.TLSv1_2
-        ctx.maximum_version = ssl.TLSVersion.TLSv1_2
-        ctx.set_ciphers('DEFAULT:@SECLEVEL=1')
-    except (AttributeError, ssl.SSLError):
-        pass
-    return ctx
-ssl.create_default_context = _patched_create_default_context
+parsed = urlparse(MONGO_URI)
+qs = parse_qs(parsed.query)
+qs['tls'] = ['true']
+qs['tlsInsecure'] = ['true']
+qs['tlsAllowInvalidCertificates'] = ['true']
+qs['tlsAllowInvalidHostnames'] = ['true']
+qs['retryWrites'] = ['true']
+qs['w'] = ['majority']
+new_query = urlencode(qs, doseq=True)
+patched_uri = urlunparse(parsed._replace(query=new_query))
 
-client = AsyncIOMotorClient(MONGO_URI, tls=True, tlsAllowInvalidCertificates=True, tlsAllowInvalidHostnames=True)
+client = AsyncIOMotorClient(patched_uri)
 db = client[DB_NAME]
