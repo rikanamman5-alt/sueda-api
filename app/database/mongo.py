b67af2fd01cf1daead20b6d425809423ff5bfaaf
Rikz Amman <rikanamman5@gmail.com>
2026-05-29 17:47:21 -1200

    fix: switch to Docker with Python 3.12 for TLS compatibility

--- app/database/mongo.py
+++ app/database/mongo.py
@@ -1,15 +1,5 @@
-from urllib.parse import urlparse, urlunparse, parse_qs, urlencode
 from motor.motor_asyncio import AsyncIOMotorClient
 from core.config import MONGO_URI, DB_NAME
 
-parsed = urlparse(MONGO_URI)
-qs = parse_qs(parsed.query)
-qs['tls'] = ['true']
-qs['tlsInsecure'] = ['true']
-qs['retryWrites'] = ['true']
-qs['w'] = ['majority']
-new_query = urlencode(qs, doseq=True)
-patched_uri = urlunparse(parsed._replace(query=new_query))
-
-client = AsyncIOMotorClient(patched_uri)
+client = AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
